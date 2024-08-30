d441d27e84bb90251d13c59c7c5a00cbb5bd5d56
FJ-AKANDE <akandefortunatus2021@gmail.com>
2024-08-30 13:42:44 +0100

    refactor: show button to load more posts but disabled when all posts have been fetched

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,11 +1,10 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { PostsContext } from "../../contexts/posts.context";
 import Card from "../../components/card/card.component";
 import Spinner from "../../components/spinner/spinner.component";
 import { IoMdAdd } from "react-icons/io";
 import { BeatLoader } from "react-spinners";
-// import { infoToast } from "../../utils/toast/toast.utils";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -18,14 +17,6 @@ const Home = () => {
     isLoading,
     error,
   } = useContext(PostsContext);
-
-  // useEffect(() => {
-  //   infoToast("toast");
-  // }, []);
-
-  // if (!hasNextPage) {
-  //   infoToast("No more posts!");
-  // }
 
   return (
     <div className="min-h-screen text-white py-24">
@@ -60,7 +51,7 @@ const Home = () => {
                   onClick={() => navigate(`/details/${post.id}`)}
                 />
               ))}
-              {hasNextPage && (
+              {hasNextPage ? (
                 <button
                   type="button"
                   className="bg-primary my-4 py-2 text-center w-full rounded-lg font-medium h-10"
@@ -74,6 +65,13 @@ const Home = () => {
                   ) : (
                     "Load More"
                   )}
+                </button>
+              ) : (
+                <button
+                  className="bg-gray-400 text-gray-700 cursor-not-allowed opacity-50 my-4 py-2 text-center rounded-lg font-medium h-10 w-full"
+                  disabled
+                >
+                  No more posts
                 </button>
               )}
             </>
